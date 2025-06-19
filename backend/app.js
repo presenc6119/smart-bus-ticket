01743e06ab174f7b9d92c19064a5e6a7f8c095c2
Farhan6Ishraq <119164785+Farhan6Ishraq@users.noreply.github.com>
2025-06-19 15:53:49 +0600

    Prepare for render

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+app.use(express.static('public'));
 let latestLocations = {}; // { busId: {lat, lng, time} }
 
 function to24Hour(time12h) {
